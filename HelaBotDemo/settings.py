f29487c34736b70a0aec6d07eb9129a0a02b6e2f
ThreeDaws <nazarmartyn2000@gmail.com>
2020-04-06 20:06:28 +0300

    have to work this time

--- HelaBotDemo/settings.py
+++ HelaBotDemo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,3 +127,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 X_FRAME_OPTIONS = 'ALLOWALL'
 
 XS_SHARING_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
+
+django_heroku.settings(locals())
